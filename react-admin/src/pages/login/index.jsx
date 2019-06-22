5b637c2299a4b41c852bd19e210bf452936549d1
xionghongyu <1562321532@qq.com>
2019-06-22 12:33:50 +0800

    完成登录请求

--- react-admin/src/pages/login/index.jsx
+++ react-admin/src/pages/login/index.jsx
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+ import React, {Component} from 'react';
 import { Form, Icon, Input, Button } from 'antd';
+import { reqLogin } from "../../api";
 
-import logo from './logo.png';
+ import logo from './logo.png';
 import './index.less';
 
 
@@ -13,27 +14,35 @@ class Login extends Component {
   login=(e)=>{
     e.preventDefault();
     //用来校验表单并获取表单的值
-    this.props.form.validateFields((error,values) =>{
-      console.log(error,values);
+    this.props.form.validateFields( async (error,values) =>{
+      // console.log(error,values);
      /* error代表表单校验
       null 校验通过
       {} 校验失败*/
      if(!error){
       //校验通过
-       const {username,password} =values;
-      console.log(username,password) ;
+       const { username,password } =values;
+       // console.log(username,password) ;
+      const result = await reqLogin( username , password );
+        if( result ){
+          this.props.history.replace('/');
+        }else{
+          this.props.form.resetFields('[password]');
+        }
+
+
      }else{
        //校验失败
        console.log('登录表单校验失败',error);
      }
     })
-
   };
 
   //自定义校验规则函数
   validator = (rule,value,callback) => {
     //callback必须调用
     // console.log(rule,value);
+    //fullField 原型上的方法， 可以传递用户名或密码
     const name = rule.fullField === 'username' ? '用户名':'密码';
     if(!value){
       //没有输入
@@ -42,8 +51,8 @@ class Login extends Component {
       callback(`${name}必须大于4位`);
     }else if(value.length>15){
       callback(`${name}必须小于15位`);
-    }else if(/^[a-zA-Z0-9]+$/.test(value)){
-      callback(!`${name}只能包含英文字母、数字、下划线`);
+    }else if(!/^[a-zA-Z0-9]+$/.test(value)){
+      callback(`${name}只能包含英文字母、数字、下划线`);
     }else{
       //不传参代表校验通过
       callback();
@@ -68,8 +77,9 @@ class Login extends Component {
                       {
                         validator:this.validator
                       }
-                    ],
-                  })(
+                    ]
+                  }
+                  )(
                   <Input className="login-input" prefix={<Icon type="user" />} placeholder="用户名" />
                 )}
             </Item>
@@ -85,7 +95,8 @@ class Login extends Component {
                         validator:this.validator
                       }
                       ],
-                  })(
+                  }
+                  )(
                     <Input className="login-input" prefix={<Icon type="lock" />} placeholder="密码" type="password"/>
               )}
             </Item>
